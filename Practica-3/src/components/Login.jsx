523a80e383b74cd80a3ef5fb895c1c1d530bf374
Joaquin Tanlongo <joako.tanlon@gmail.com>
2024-09-06 19:45:26 -0300

    Practica 3 terminada

--- Practica-3/src/components/Login.jsx
+++ Practica-3/src/components/Login.jsx
@@ -7,14 +7,15 @@ const Login = () => {
     const handleChange = (e) => {
 
         let newUserName = e.target.value;
+        if(e.target.value.includes('o') || e.target.value.includes('O')){
+            alert("Por favor, ¡Nombres de usuario sin la letra o!");
+        }
         setUsername(newUserName);
     }
 
     const handleRegister = () => {
 
-        if(username.includes('o') || username.includes('O')){
-            alert("Usuario inválido para registrarse");
-        } else if (username.trim() === ""){
+        if(username.includes('o') || username.includes('O') || username.trim() === ""){
             alert("Usuario inválido para registrarse");
         }else {
             alert("Usuario registrado correctamente");
